a5ad4e3a1e9e928f30028939fd5d920e0603b149
Dalton Menezes <daltonmenezes@outlook.com>
2018-11-10 00:07:10 -0300

    Update button css to nle-button class & append that class in buttonRender function

--- chrome/content_scripts/ui/button-render.js
+++ chrome/content_scripts/ui/button-render.js
@@ -2,6 +2,7 @@ const buttonRender = ({ name, i18n }) => {
   let button = document.createElement(name)
 
   button.textContent = chrome.i18n.getMessage(i18n)
-
+  button.classList.add('nle-button')
+  
   document.querySelector('.galleryHeader').appendChild(button)
 }
--- firefox/content_scripts/ui/button-render.js
+++ firefox/content_scripts/ui/button-render.js
@@ -2,7 +2,8 @@ const buttonRender = ({ name, i18n }) => {
   let button = document.createElement(name)
 
   button.textContent = browser.i18n.getMessage(i18n)
-
+  button.classList.add('nle-button')
+  
   document.querySelector('.galleryHeader').appendChild(button)
 }
 
