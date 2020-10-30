d1aef2a7b62f55fbd9e10c43ce2060aaa4011a88
yuanLeeMidori <midori3113133@gmail.com>
2020-10-30 15:32:23 -0400

    Fix: sort exported list (update thumbs-list-creator.js)

--- firefox/content_scripts/creators/my-list-creator.js
+++ firefox/content_scripts/creators/my-list-creator.js
@@ -6,7 +6,6 @@ const myListCreator = () => {
     .forEach(name => name.getAttribute('aria-label') !== null ? list += `${name.getAttribute('aria-label')}\n` : '')
  
   list = [...new Set(list.split('\n').filter(Boolean))].sort(Intl.Collator().compare).join('\n')
-
   return list  
 }
 
